2c2558c601d671b8e3b80993dd8a810c7ca21eaa
Đinh Sỹ Dũng <dungbn2000@gmail.com>
2023-03-30 16:57:02 +0700

    update

--- src/component/content/content.jsx
+++ src/component/content/content.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./content.scss";
-import Demo from "../../assets/img/anh_demo.jpg";
+import Demo from "../../assets/img/avatar.jpg";
 import react from "../../assets/react.svg";
 import html from "../../assets/html.png";
 import css from "../../assets/css.png";
